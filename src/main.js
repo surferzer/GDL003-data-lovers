111f37f802cea37bc3dc63dcd9f58cf6a627fdf7
surferzer <mafernandazr@gmail.com>
2019-06-24 19:33:18 -0500

    Agregando funcionalidad al boton RICKYMORTY, KNOWMORE para el cambio de pantallas

--- src/main.js
+++ src/main.js
@@ -1,11 +1,51 @@
-window.example={
-filtrado: (results, status) =>{
-const y=0
-const alive= "results".status.value
+//window.example={
+//filtrado: (results, status) =>{
+//const y=0
+//const alive= "results".status.value
 
-for(i=0; i<results.length; i++){
+//for(i=0; i<results.length; i++){
 
-if(results.status[i].value="Alive"){
-  y= y+1}
-}}};
-console.log(alive)
+//if(results.status[i].value="Alive"){
+//  y= y+1}
+//}}};
+//console.log(alive)
+
+//INICIO//
+
+welcome.style.display="block";
+rick.style.display="none";
+lookFor.style.display="none";
+
+
+//FUNCION PAGINA 1//
+function start(){
+welcome.style.display="none";
+rick.style.display="block";
+lookFor.style.display="none";
+}
+
+function knowMore(){
+welcome.style.display="none";
+rick.style.display="none";
+lookFor.style.display="block";
+}
+
+function name(){}
+
+function death(){}
+
+function alive(){}
+
+function aliens(){}
+
+function earth(){}
+
+function women(){}
+
+function men(){}
+
+function humans(){}
+
+function episode(){}
+
+function back(){}
